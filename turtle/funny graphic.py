4289c085ffbc999610617b13fa9ee915b34f1a5b
kaleb <hkaleb290@tamu.edu>
2025-09-25 11:42:16 -0500

    big pp

--- turtle/funny graphic.py
+++ turtle/funny graphic.py
@@ -1,12 +1,22 @@
 import turtle
-
+CARD_WIDTH = 50
+CARD_HEIGHT = 70
 screen = turtle.Screen()
-screen.bgcolor("black")
-for _ in range(500):
+screen.bgcolor("Green")
+for _ in range(100):
     turtle.forward(300)
     turtle.left(260)
 
-def draw():
-    turtle.forward(300)
-    turtle.left(260)
-
+def draw_card_front(t, x, y, card):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.color("white", "white")
+    t.begin_fill()
+    t.setheading(0)
+    for _ in range(2):
+        t.forward(CARD_WIDTH)
+        t.right(90)
+        t.forward(CARD_HEIGHT)
+        t.right(90)
+    t.end_fill()
