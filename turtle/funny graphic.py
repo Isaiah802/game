f9daac1365c22c4523ff4e1c088c51efcfa7bfc4
kaleb <hkaleb290@tamu.edu>
2025-09-25 10:44:54 -0500

    funny

--- turtle/funny graphic.py
+++ turtle/funny graphic.py
@@ -1,7 +1,12 @@
 import turtle
 
+screen = turtle.Screen()
+screen.bgcolor("black")
+for _ in range(500):
+    turtle.forward(300)
+    turtle.left(260)
 
 def draw():
-    turtle.forward(100)
-    turtle.left(90)
+    turtle.forward(300)
+    turtle.left(260)
 
