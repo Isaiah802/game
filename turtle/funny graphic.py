de6592416e245926552a4c2f194ff8abfd171f5c
kaleb <hkaleb290@tamu.edu>
2025-09-25 12:09:09 -0500

    l

--- turtle/funny graphic.py
+++ turtle/funny graphic.py
@@ -3,26 +3,13 @@ CARD_WIDTH = 50
 CARD_HEIGHT = 70
 screen = turtle.Screen()
 screen.bgcolor("Green")
-for _ in range(100):
-    turtle.forward(300)
-    turtle.left(260)
-def create_deck():
-    """Creates a standard 5-card deck."""
-def draw_card_front(t, x, y, card):
-    t.penup()
-    t.goto(x, y)
-    t.pendown()
-    t.color("white", "white")
-    t.begin_fill()
-    t.setheading(0)
-    for _ in range(2):
-        t.forward(CARD_WIDTH)
-        t.right(260)
-        t.forward(CARD_HEIGHT)
-        t.right(100)
-        t.end_fill()
- def play_round(self):
-        # 1. Setup
-        self.deck = create_deck()
-        for player in self.players:
-            player.card_turtle.clear()
+def draw_card():
+    card = turtle.Turtle()
+    card.shape("square")
+    card.color("red")
+    card.penup()
+    card.goto(-CARD_WIDTH/2, -CARD_HEIGHT/2)
+    card.pendown()
+    card.begin_fill()
+    card.goto(-CARD_WIDTH/2, -CARD_HEIGHT/2)
+
