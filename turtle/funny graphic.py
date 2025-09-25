6789fee585e277f738367280d58b5ff8453b332c
kaleb <hkaleb290@tamu.edu>
2025-09-25 12:00:53 -0500

    l

--- turtle/funny graphic.py
+++ turtle/funny graphic.py
@@ -6,7 +6,8 @@ screen.bgcolor("Green")
 for _ in range(100):
     turtle.forward(300)
     turtle.left(260)
-
+def create_deck():
+    """Creates a standard 5-card deck."""
 def draw_card_front(t, x, y, card):
     t.penup()
     t.goto(x, y)
@@ -16,7 +17,12 @@ def draw_card_front(t, x, y, card):
     t.setheading(0)
     for _ in range(2):
         t.forward(CARD_WIDTH)
-        t.right(90)
+        t.right(260)
         t.forward(CARD_HEIGHT)
-        t.right(90)
-    t.end_fill()
+        t.right(100)
+        t.end_fill()
+ def play_round(self):
+        # 1. Setup
+        self.deck = create_deck()
+        for player in self.players:
+            player.card_turtle.clear()
